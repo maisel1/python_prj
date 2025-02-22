dc426ec72ec2e58c9f9f6e38ca1cc3c8488e0f2a
maisel <maikoltondini@gmail.com>
2025-02-22 19:12:51 +0100

    Logic implemented

--- Sasso carta forbici/#ROCK-PAPER-SCISSORS.py
+++ Sasso carta forbici/#ROCK-PAPER-SCISSORS.py
@@ -1,10 +1,51 @@
 #ROCK-PAPER-SCISSORS
 import random
 
-player_choice = input("""Welcome to Rock, Paper, scissors terminal game.
+#taking player chice
+player_choice = int(input("""Welcome to Rock, Paper, scissors terminal game.
       Chose:
       1) for rock;
       2) for paper
-      3) for scissors""")
+      3) for scissors
+      """))
+#taking computer chouce
+computer_choice = random.randint(1, 3)
 
-computer_choice = 
+#printing player's and computer's selected tool
+if computer_choice == 1:
+    print("computer_chouce is ROCK")
+elif computer_choice == 2:
+    print("computer choice is PAPER")
+elif computer_choice == 3:
+    print("computer choice is SCISSORS")
+    
+if player_choice == 1:
+    print("your choice is ROCK")
+elif player_choice == 2:
+    print("your choice is PAPER")
+elif player_choice == 3:
+    print("your choice is SCISSORS")
+else:
+    print("you chose... VIOLENCE!! (only numbers from 1 to 3 are valid)")
+    
+#define the winner
+
+if computer_choice == player_choice:
+    print("it's a tie")
+elif computer_choice == 1 and player_choice == 2:
+    print("you won!")
+elif computer_choice == 1 and player_choice == 3:
+    print("you lose!")
+elif computer_choice == 2 and player_choice == 1:
+    print("you lose!")
+elif computer_choice == 2 and player_choice == 3:
+    print("you won!")
+elif computer_choice == 2 and player_choice == 3:
+    print("you won!")
+elif computer_choice == 3 and player_choice == 1:
+    print("you won!")
+elif computer_choice == 3 and player_choice == 2:
+    print("you lose!")
+else:
+    print("there's something wrong")   
+
